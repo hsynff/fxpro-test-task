0c7a803d9e266bb767d8e49c38eb504555f78174
Joshgun Huseynov <cosqunhsynv@gmail.com>
2020-03-02 01:24:23 +0400

    test initial

--- src/test/java/com/fxpro/task/AppTest.java
+++ src/test/java/com/fxpro/task/AppTest.java
@@ -1,20 +1,10 @@
 package com.fxpro.task;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+
+//    public void
+
 }
